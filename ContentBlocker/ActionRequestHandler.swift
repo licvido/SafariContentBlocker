e36923a182e311b4eccb4dec4656a9f2529fd7c9
Filip Mikovcak <fmikovcak@gmail.com>
2015-09-22 20:49:45 +0200

    Add application extension

---
+++ ContentBlocker/ActionRequestHandler.swift
@@ -0,0 +1,23 @@
+//
+//  ActionRequestHandler.swift
+//  ContentBlocker
+//
+//  Created by Filip Mikovcak on 22.09.15.
+//  Copyright © 2015 Evasio. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ActionRequestHandler: NSObject, NSExtensionRequestHandling {
+
+    func beginRequestWithExtensionContext(context: NSExtensionContext) {
+        let attachment = NSItemProvider(contentsOfURL: NSBundle.mainBundle().URLForResource("blockerList", withExtension: "json"))!
+    
+        let item = NSExtensionItem()
+        item.attachments = [attachment]
+    
+        context.completeRequestReturningItems([item], completionHandler: nil);
+    }
+    
+}
